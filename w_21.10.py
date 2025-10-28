cbbea6da56b461c7050d3995179e975609f8da6f
28.10.2025 <89527986414@bk.ru>
2025-10-28 19:49:45 +0200

    12

---
+++ w_21.10.py
@@ -0,0 +1,29 @@
+#Задача 1
+# a= input()
+# print(a[::-1])
+
+#Задача 2
+# a= input()
+# print(a[0::2])
+
+#Задача 3
+# a=input()
+# print(a[0:5]+a[-5:])
+
+#Задача 4
+# a=input()
+# print(a.rfind(a[-1])+1)
+
+#Задача 5
+# a=input()
+# x=a.find('o')
+# if x==-1:
+#     print("буква \"о\" отсутствует")
+# else:
+#     print(x)
+
+#Задача 6
+# a=input()
+# print(a.rfind('a'))
+
+
