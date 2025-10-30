dd5f637f4452cdd6dedb45ec6ff0740813253633
28.10.2025 <89527986414@bk.ru>
2025-10-30 19:44:41 +0200

    12

---
+++ 30.10.2025.py
@@ -0,0 +1,34 @@
+#Задача 1
+# a=input()
+# if a[-2::]=="ов":
+#     print("yes")
+# else:
+#     print("no")
+
+#Задача 2
+# a=input()
+# s="1234567890"
+# z=''
+# for i in a:
+#     for j in s:
+#         if i==j:
+#             z="yes"
+#             break
+#         else:
+#             z="no"
+# print(z)
+
+#Задача 3
+# a=input()
+# a=a.replace('',' ')
+# a=a.strip()
+# print(a.replace(' ',','))
+
+#Задача 4
+# a=input()
+# s="eyuioaEYUIOAуеыаоэяиюУЕЫАОЭЯИЮ"
+# for i in a:
+#     for j in s:
+#         if i == j:
+#             a=a.replace(j,"")
+# print(a)
