b9121660e313b97cb9ab1640726d65970365fba5
12 <89527986414@bk.ru>
2026-01-10 16:35:57 +0200

    12

---
+++ 10,01,2026.py
@@ -0,0 +1,24 @@
+#Задание 1
+# keys = ['name', 'age', 'city', 'profession']
+# values = ['Иван', 28, 'Москва', 'Программист']
+# dict=dict(zip(keys, values))
+# for key, value in enumerate(dict.items(), start=1):
+#     print(key, value)
+
+#Задание 2
+# correct_answers = ['A', 'B', 'C', 'D', 'A']
+# student_answers = ['A', 'B', 'D', 'D', 'A']
+# count=0
+# c=0
+# for cor, st in zip(correct_answers, student_answers):
+#     c+=1
+#     if cor == st:
+#         count+=1
+# print("Верно решено:",count,"из",c)
+
+#Задание 4
+# list= [20, 22, 18, 25, 19, 21]
+# c=[]
+# for i,t in enumerate(list, start=0):
+#     c.append(t+i)
+# print(c)
